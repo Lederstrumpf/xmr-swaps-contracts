cb5a1d8d382f986fb70dedd7ed912814800d6321
Aleixo Sánchez <15819210+alxs@users.noreply.github.com>
2021-10-24 03:19:06 +0200

    more testing, use alternative ed25519 implementation

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -2,7 +2,8 @@
 
 pragma solidity 0.8.5;
 
-import "./Ed25519.sol";
+// import "./Ed25519.sol";
+import "./Ed25519_alt.sol";
 import "hardhat/console.sol";
 
 contract Swap {
@@ -95,8 +96,11 @@ contract Swap {
     }
 
     function verifySecret(uint256 _s, bytes32 pubKey) public view {
-        (uint256 px, uint256 py) = ed25519.derivePubKey(_s);
-        uint256 canonical_p = py | ((px >> (8 * 30 + 1)) & 0x80);
+        // (uint256 px, uint256 py) = ed25519.derivePubKey(_s);
+        (uint256 px, uint256 py) = ed25519.scalarMultBase(_s);
+        uint256 canonical_p = py | ((px % 2) << 255);
+        console.log("py: %s", uint2hexstr(py));
+        console.log("px: %s", uint2hexstr(px));
         console.log("derived:  %s", uint2hexstr(canonical_p));
         console.log("provided: %s", uint2hexstr(uint256(pubKey)));
         require(
--- test/generate_pubKey.js
+++ test/generate_pubKey.js
@@ -2,59 +2,34 @@ const { expect } = require("chai");
 const ed = require("noble-ed25519");
 
 describe("Swap", function () {
-  // function toHexString(byteArray) {
-  //   return Array.from(byteArray, function (byte) {
-  //     return ('0' + (byte & 0xFF).toString(16)).slice(-2);
-  //   }).join('')
-  // }
-
+  function toHexString(byteArray) {
+    return Array.from(byteArray, function (byte) {
+      return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('')
+  }
+  function hexToBytes(hex) {
+    for (var bytes = [], c = 0; c < hex.length; c += 2)
+      bytes.push(parseInt(hex.substr(c, 2), 16));
+    return bytes;
+  }
+  
   it("Should generate public key correctly", async function () {
     // const s_a = ed.utils.randomPrivateKey();
     // console.log(toHexString(s_a));
+    const s_a = '34c388ea5bdd494b10b1eaebfa68564463947ee48efe69c520d4a1fadc550c04';
     // const s_b = ed.utils.randomPrivateKey();
     // console.log(toHexString(s_b));
-    // const pubKey_a = await ed.getPublicKey(s_a);
-    // console.log(toHexString(pubKey_a));
-    // const pubKey_b = await ed.getPublicKey(s_b);
+    const s_b = 'a6e51afb9662bf2173d807ceaf88938d09a82d1ab2cea3eeb1706eeeb8b6ba03';
+    const pubKey_a = 'e32ad36ce8e59156aa416da9c9f41a7abc59f6b5f1dd1c2079e8ff4e14503090';
+    // console.log(pubKey_a);
+    const pubKey_b = '57edf916a28c2a0a172565468564ab1c5c217d33ea63436f7604a96aa28ec471';
     // console.log(toHexString(pubKey_b));
 
-    const s_a = [
-      66, 122, 84, 24, 129, 247, 125, 184,
-      103, 8, 24, 113, 104, 18, 80, 150,
-      123, 246, 135, 119, 168, 233, 196, 222,
-      109, 103, 72, 170, 62, 171, 141, 8
-    ];
-    const s_b = [
-      32, 40, 49, 87, 254, 232, 89, 239,
-      50, 110, 6, 57, 86, 71, 95, 69,
-      189, 176, 117, 106, 165, 34, 250, 124,
-      166, 47, 137, 109, 94, 102, 210, 12
-    ];
-    const pubKey_a = [
-      102, 131, 157, 191, 81, 33, 222,
-      165, 216, 25, 86, 206, 162, 233,
-      88, 14, 189, 17, 199, 187, 171,
-      111, 101, 153, 166, 62, 107, 100,
-      136, 14, 212, 174
-    ];
-    const pubKey_b = [
-      64, 18, 89, 215, 102, 66, 239, 204,
-      170, 132, 153, 42, 237, 8, 162, 97,
-      68, 214, 200, 207, 27, 129, 142, 194,
-      80, 84, 0, 131, 99, 47, 226, 181
-    ];
-
     const Swap = await ethers.getContractFactory("Swap");
-    const swap = await Swap.deploy(pubKey_a, pubKey_b);
+    const swap = await Swap.deploy(hexToBytes(pubKey_a).reverse(), hexToBytes(pubKey_b).reverse());
     await swap.deployed();
 
-    await swap.verifySecret(s_a, pubKey_a);
-
-    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // // wait until the transaction is mined
-    // await setGreetingTx.wait();
-
-    // expect(await greeter.greet()).to.equal("Hola, mundo!");
+    await swap.verifySecret(hexToBytes(s_a).reverse(), hexToBytes(pubKey_a).reverse());
+    await swap.verifySecret(hexToBytes(s_b).reverse(), hexToBytes(pubKey_b).reverse());
   });
 });
